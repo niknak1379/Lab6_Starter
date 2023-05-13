58a7d77b0c5f4040b7a1730aaebe4ae659fb7752
Nikan Ostovan <nikanostovan@gmail.com>
2023-05-12 19:39:30 -0700

    finished explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,7 +24,7 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
-  if (localStorage.length == 0 ) {
+  if ( localStorage.length == 0 ) {
     return [];
   }
   let localObject = localStorage.getItem('recipes');
